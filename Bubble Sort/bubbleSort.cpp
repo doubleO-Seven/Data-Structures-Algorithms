049040f83bf8be1e08096bf46c7a90df7a8dc16a
S M Shakib Hossain <smsakib2403@gmail.com>
2022-02-09 00:47:10 +0600

    Create bubbleSort.cpp

---
+++ Bubble Sort/bubbleSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of an array: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter the array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int counter = 1;
+    while(counter < n)
+    {
+        for(int i = 0; i< n-counter; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        counter++;
+    }
+    
+    cout<<"Sorted array is: ";
+    for(int i=0; i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
